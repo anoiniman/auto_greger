ce4124b13d38b662bb464d4f80d330964ee599bb
anoiniman <44550563+anoiniman@users.noreply.github.com>
2025-04-29 01:58:14 +0100

    all that could be done today, kicking the rel_movement can down the road and praying that the needed structure will spontanously emerge from this action

--- robot/nav_module/rel_move.lua
+++ robot/nav_module/rel_move.lua
@@ -1,6 +1,12 @@
+local nav = require("nav_module.nav_interface")
 local goal_rel = {0,0,0} -- x,z,y
 
-function module.setup_navigate_rel(to_where, nav_obj)
-
+function module.setup_navigate_rel(x,z,y)
+    goal_rel[1] = x
+    goal_rel[2] = z
+    goal_rel[3] = y
 end
 
+function module.navigate_rel(nav_obj)
+
+end
