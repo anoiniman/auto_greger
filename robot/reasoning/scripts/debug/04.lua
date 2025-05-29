4ea10ee9f362830013a1430ebb05e90db8b09f71
anoiniman <44550563+anoiniman@users.noreply.github.com>
2025-05-29 23:32:27 +0100

    small fix

--- robot/reasoning/scripts/debug/04.lua
+++ robot/reasoning/scripts/debug/04.lua
@@ -6,7 +6,7 @@ local gravel_only, _ = require("reasoning.recipes.stone_age.gathering01")
 local desc = "Debug 04 - Get gathering me boy"
 local builder = MSBuilder:new_w_desc(desc)
 
-local constraint = Constraint:newItemConstraint("Gravel", 32, nil)
+local constraint = Constraint:newItemConstraint(nil, "Gravel", 32, nil)
 local simple_goal = Goal:new(nil, constraint, 40, nil, false)
 builder:addGoal(simple_goal)
 builder:addRecipe(gravel_only)
