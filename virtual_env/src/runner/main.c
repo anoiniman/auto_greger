64b05ef9d56b466600b2a3954721e50146abd3b7
anoiniman <44550563+anoiniman@users.noreply.github.com>
2025-12-23 18:51:18 +0000

    Revert "Hacking" :P
    This reverts commit 811ff7ddd34e90b9af3cca658cbc9d7df478494c.

--- virtual_env/src/runner/main.c
+++
@@ -1,6 +0,0 @@
-// #include "render.h"
-
-int main (int argc, char *argv[]) {
-    printf("Hello World!");
-    return 0;
-}
---
+++ virtual_env/src/stencil.d
@@ -0,0 +1,5 @@
+src/stencil.o: src/stencil.c \
+ ../../../raylib-5.5_linux_amd64/include/rlgl.h \
+ ../../../raylib-5.5_linux_amd64/include/GLFW/glfw3.h
+../../../raylib-5.5_linux_amd64/include/rlgl.h:
+../../../raylib-5.5_linux_amd64/include/GLFW/glfw3.h:
